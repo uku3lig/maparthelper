a09c94cc415c373b293d462285b0c92923bbad55
Uku3lig <thelololomail@gmail.com>
2022-05-30 17:14:14 +0200

    fix argparse

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ QUASI_PRIMARY_DYES = ["Light Blue", "Light Gray", "Lime", "Magenta", "Orange", "
 def print_item(name: str, padding: int, count: int) -> None:
     s = f'{name:<{padding}}  '
 
-    if args.p == 'shulker' and (args.strict or (count < 1728 and not args.l) or count >= 1728):
+    if args.precision == 'shulker' and (args.strict or (count < 1728 and not args.lower) or count >= 1728):
         s += f"{(n := math.ceil(count / 1728))} shulker{'s' if n > 1 else ''}"
         count = 0
     elif count >= 1728 and not args.strict:
         s += f"{(n := count // 1728)} shulker{'s' if n > 1 else ''} "
         count %= 1728
     
-    if args.p == 'stack' and (args.strict or (count < 64 and not args.l) or count >= 64):
+    if args.precision == 'stack' and (args.strict or (count < 64 and not args.lower) or count >= 64):
         s += f"{(n := math.ceil(count / 64))} stack{'s' if n > 1 else ''}"
         count = 0
     elif count >= 64 and not args.strict:
@@ -85,15 +85,15 @@ def add(d: Dict[str, int], keys: List[str], amount: int) -> None:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', help='path to the csv file containing the material list')
-parser.add_argument('-p', choices=['shulker', 'stack', 'item'], default='stack', help='lowest precision of the values')
-parser.add_argument('-l', help='if values are lower than the precision, display them more precisely', action='store_true')
-parser.add_argument('--strict', help='keep all values in the defined precision', action='store_true')
+parser.add_argument('--precision', '-p', choices=['shulker', 'stack', 'item'], default='stack', help='lowest precision of the values')
+parser.add_argument('--lower', '-l', help='if values are lower than the precision, display them more precisely', action='store_true')
+parser.add_argument('--strict', '-S', help='keep all values in the defined precision', action='store_true')
 parser.add_argument('--dye', '-d', help='compute the amount of dye needed', choices=['all', 'quasi', 'primary'], default=None)
 
 args = parser.parse_args()
 
-if args.l and args.strict:
-    print("-l and --strict are mutually exclusive. Exiting.")
+if args.lower and args.strict:
+    print("--lower and --strict are mutually exclusive. Exiting.")
     sys.exit()
 
 data = pd.read_csv(args.file, usecols=['Item', 'Total'], index_col=0).squeeze('columns').to_dict()
